3d127b1da1bcb9ae30c46f0227267de2c934aa9e
Elfyndor <elvin.x.li203@gmail.com>
2025-09-14 02:00:45 -0400

    add solution to git

--- ECS/include/zOldruntime/winMain.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <Windows.h>
-#include "myStandards.h"
-#include "gameLoop.h"
-
-#include "messageHandler.h"
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow);
